70d5e6c6239ae7ff763b1bb4f2704ef18f36b75c
TimRiedel <timriedel00@gmail.com>
2026-01-06 15:56:14 +0100

    fix: make application executable with configuration changes

--- src/main.py
+++ src/main.py
@@ -5,8 +5,38 @@ import torch
 
 from trainer import Trainer
 from data import *
+from models import *
 
+# Enable Tensor Core optimization for better performance on CUDA devices with Tensor Cores
+torch.set_float32_matmul_precision('high')
 logger = logging.getLogger(__name__)
+
+def train(cfg: DictConfig, input_seq_len: int, horizon_seq_len: int) -> None:
+    trainer = Trainer(cfg["trainer"], cfg["callbacks"], cfg["wandb"])
+    trainer.logger.config = cfg
+
+    data = AerionData(cfg["dataset"], cfg["data_processing"], cfg["dataloader"], cfg.seed)
+
+    model = TransformerModule(
+        cfg["model"],
+        cfg["optimizer"],
+        input_seq_len,
+        horizon_seq_len,
+        data.norm_mean,
+        data.norm_std
+    )
+    trainer.fit(model, data)
+
+
+def test(cfg: DictConfig, input_seq_len: int, horizon_seq_len: int) -> None:
+    # TODO: When loading model from checkpoint, obtain norm_mean and norm_std from the checkpoint
+    # and pass it to the datamodule for correct normalization data loading transforms
+    raise NotImplementedError("Test stage not implemented. Loading model checkpoints is not supported yet.")
+
+
+def calculate_seq_len(time_minutes: int, resampling_rate_seconds: int) -> int:
+    return time_minutes * 60 // resampling_rate_seconds
+
 
 @hydra.main(version_base=None, config_path="../configs", config_name="execute_aerion")
 def main(cfg: DictConfig) -> None:
@@ -16,10 +46,14 @@ def main(cfg: DictConfig) -> None:
         torch.manual_seed(cfg.seed)
         torch.cuda.manual_seed_all(cfg.seed)
 
-    trainer = Trainer(cfg["trainer"], cfg["callbacks"], cfg["wandb"])
-    trainer.logger.config = cfg
+    input_seq_len = calculate_seq_len(cfg["dataset"]["input_time_minutes"], cfg["dataset"]["resampling_rate_seconds"])
+    horizon_seq_len = calculate_seq_len(cfg["dataset"]["horizon_time_minutes"], cfg["dataset"]["resampling_rate_seconds"])
 
-    data = AerionData(cfg["dataset"], cfg["data_processing"], cfg["dataloader"], cfg.seed)
+    if cfg.stage == "train" or cfg.stage == "fit":
+        train(cfg, input_seq_len, horizon_seq_len)
+    elif cfg.stage == "test":
+        test(cfg, input_seq_len, horizon_seq_len)
+
 
 if __name__ == "__main__":
     main()
--- src/trainer.py
+++ src/trainer.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 import warnings
 from typing import List
 from omegaconf import DictConfig
 from hydra.utils import instantiate
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 
 warnings.filterwarnings("ignore", message=".*srun.*")
@@ -17,8 +19,38 @@ class Trainer(pl.Trainer):
         
     def _setup_callbacks(self, callbacks_cfg: DictConfig) -> List[pl.Callback]:
         callbacks = []
-        for callback_cfg in callbacks_cfg.values():
-            callbacks.append(instantiate(callback_cfg))
+
+        if callbacks_cfg.get("early_stopping", None) is not None:
+            early_stopping_cfg = callbacks_cfg["early_stopping"]
+            early_stopping = EarlyStopping(
+                monitor=early_stopping_cfg.get("monitor", "val_loss"),
+                mode=early_stopping_cfg.get("mode", "min"),
+                patience=early_stopping_cfg.get("patience", 10),
+                min_delta=early_stopping_cfg.get("min_delta", 0.0)
+            )
+            callbacks.append(early_stopping)
+
+        if callbacks_cfg.get("learning_rate_monitor", None) is not None:
+            learning_rate_monitor_cfg = callbacks_cfg["learning_rate_monitor"]
+            learning_rate_monitor = LearningRateMonitor(
+                logging_interval=learning_rate_monitor_cfg.get("logging_interval", "epoch")
+            )
+            callbacks.append(learning_rate_monitor)
+
+        if callbacks_cfg.get("checkpoint", None) is not None:
+            checkpoint_cfg = callbacks_cfg["checkpoint"]
+            dirpath = checkpoint_cfg.get("dirpath", ".outputs/checkpoints")
+            dirpath = dirpath + f"/{datetime.now().strftime('%Y-%m-%d')}/{datetime.now().strftime('%H-%M-%S')}"
+            checkpoint = ModelCheckpoint(
+                dirpath=dirpath,
+                filename=checkpoint_cfg.get("filename", "{epoch}-{val_loss:.2f}"),
+                monitor=checkpoint_cfg.get("monitor", "val_loss"),
+                mode=checkpoint_cfg.get("mode", "min"),
+                save_top_k=checkpoint_cfg.get("save_top_k", 1),
+                save_last=checkpoint_cfg.get("save_last", True)
+            )
+            callbacks.append(checkpoint)
+
         return callbacks
 
     def _setup_logger(self, logger_cfg: DictConfig) -> WandbLogger:
