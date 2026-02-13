f20b37315b24109efdbb79dd612fdd3c73f92834
TimRiedel <timriedel00@gmail.com>
2026-02-13 13:10:54 +0100

    feat: add method for converting position to runway relative coordinates

--- src/data/utils/__init__.py
+++ src/data/utils/__init__.py
@@ -1,5 +1,5 @@
 from data.utils.trajectory import reconstruct_absolute_from_deltas
-from data.utils.runway import compute_extended_centerline_point, compute_dx_dy_bearing, get_distances_to_centerline, construct_runway_features
+from data.utils.runway import compute_extended_centerline_point, compute_dx_dy_bearing, get_distances_to_centerline, construct_runway_features, convert_pos_to_rwy_coordinates
 from data.utils.projections import get_transformer_wgs84_to_aeqd, get_transformer_aeqd_to_wgs84
 
 __all__ = [
@@ -8,6 +8,7 @@ __all__ = [
     "compute_extended_centerline_point",
     "get_distances_to_centerline",
     "construct_runway_features",
+    "convert_pos_to_rwy_coordinates",
     "get_transformer_wgs84_to_aeqd",
     "get_transformer_aeqd_to_wgs84",
 ]
